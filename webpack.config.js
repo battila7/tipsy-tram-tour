e25cf9d1c56d7ba1731299ef09b96255984202b3
Attila Bagossy <bagossyattila@outlook.com>
2017-12-03 11:28:17 +0100

    Init'd Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
+const config = {
+    context: srcPath,
+    entry: {
+      app: './js/main.js',
+    },
+    output: {
+      path: path.join(distPath, 'assets'),
+      publicPath: '/assets/',
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(srcPath, 'css/**'),
+          to: path.join(distPath, 'assets')
+        },
+        {
+          context: path.join(srcPath, 'html'),
+          from: '*.html',
+          to: distPath 
+        }
+      ])
+    ]
+};
+
+module.exports = config;
