f83997ac876dd1538d306df7de3cc3d74881de3b
Attila Bagossy <bagossyattila@outlook.com>
2017-12-04 18:41:25 +0100

    Wrapped the Google Distance Matrix API

--- src/js/distance-calculator.js
+++ src/js/distance-calculator.js
@@ -1,7 +1,40 @@
-const DistanceCalculator = {
+const walkingTravelMode = 'WALKING';
 
+const DistanceCalculator = {
+    DistanceCalculator({ service, successStatus }) {
+        this.service = service;
+        this.successStatus = successStatus;
+    },
+    distanceBetween(from, to) {
+        const fromLatLng = this.toLatLng(from);
+        const toLatLng = this.toLatLng(to);
+
+        return new Promise((resolve, reject) => {
+            this.service.getDistanceMatrix({
+                origins: [fromLatLng],
+                destinations: [toLatLng],
+                travelMode: walkingTravelMode
+            }, (response, status) => {
+                if (status == this.successStatus) {
+                    resolve(response.rows[0]);
+                } else {
+                    reject(status);
+                }
+            });
+        });
+    },
+    toLatLng(location) {
+        return new google.maps.LatLng(location.lat, location.lng);
+    }
 };
 
+const distanceCalculator = Object.create(DistanceCalculator);
+distanceCalculator.DistanceCalculator({
+    service: new google.maps.DistanceMatrixService(),
+    successStatus: 'OK'
+});
+
 module.exports = {
+    distanceCalculator,
     DistanceCalculator
 };
