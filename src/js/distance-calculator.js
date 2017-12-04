0e2e6bab46089e36685e53566c88360e279a46aa
Attila Bagossy <bagossyattila@outlook.com>
2017-12-04 13:34:31 +0100

    Restyled the map with kinda ugly code

---
+++ src/js/distance-calculator.js
@@ -0,0 +1,7 @@
+const DistanceCalculator = {
+
+};
+
+module.exports = {
+    DistanceCalculator
+};
--- src/js/publine.js
+++ src/js/publine.js
@@ -4,8 +4,6 @@ module.exports = function populate(pubs) {
     for (content of document.querySelectorAll('.publine-content')) {
         const pub = pubIterator.next().value;
 
-        console.log(pub);
-
         content.querySelector('.vicinity').innerHTML = pub.vicinity;
 
         content.querySelector('.pub-rating > span').innerHTML = pub.rating;
